17c54dd6a1f13d7b342497de6f511f4b55742822
Geovanni Roberts <geovanniroberts@Geovannis-MacBook-Pro-2.local>
2022-12-28 16:20:14 -0500

    seventh

--- pkg/service/v1/todo-service.go
+++ pkg/service/v1/todo-service.go
@@ -6,32 +6,34 @@ import (
 	"fmt"
 	"time"
 
+	v1 "github.com/Geo881/go-grpc-http-rest/pkg/api/v1"
 	"github.com/golang/protobuf/ptypes"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
-
-	v1 "github.com/Geo881/go-grpc-http-rest/pkg/api/v1"
 )
 
 const (
+	// apiVersion is version of API is provided by server
 	apiVersion = "v1"
 )
 
+// toDoServiceServer is implementation of v1.ToDoServiceServer proto interface
 type toDoServiceServer struct {
 	db *sql.DB
 }
 
-func NewToDoServiceServer(db *sql.DB) v1.toDoServiceServer {
+// NewToDoServiceServer creates ToDo service
+func NewToDoServiceServer(db *sql.DB) v1.ToDoServiceServer {
 	return &toDoServiceServer{db: db}
 }
 
+// checkAPI checks if the API version requested by client is supported by server
 func (s *toDoServiceServer) checkAPI(api string) error {
 	// API version is "" means use current version of the service
 	if len(api) > 0 {
 		if apiVersion != api {
 			return status.Errorf(codes.Unimplemented,
-				`unsupported API version: service implements API
-				version '%s', but asked for '%s'`, apiVersion, api)
+				"unsupported API version: service implements API version '%s', but asked for '%s'", apiVersion, api)
 		}
 	}
 	return nil
@@ -41,8 +43,7 @@ func (s *toDoServiceServer) checkAPI(api string) error {
 func (s *toDoServiceServer) connect(ctx context.Context) (*sql.Conn, error) {
 	c, err := s.db.Conn(ctx)
 	if err != nil {
-		return nil, status.Error(codes.Unknown, `failed to connect to 
-		database-> `, err.Error())
+		return nil, status.Error(codes.Unknown, "failed to connect to database-> "+err.Error())
 	}
 	return c, nil
 }
